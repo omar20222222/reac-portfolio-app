105bc40ea61a1ee659a20dd151d759d7c51763c1
omar20222222 <108297683+omar20222222@users.noreply.github.com>
2022-12-07 20:52:49 +0000

    added Folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/HomeHome";
+import About from "./components/About";
+import Projects from "./components/Project";
+import footer from "./components/footer";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+{
+  return (
+    <Router>
+      {load ? (
+        <Preloader load={load} />
+      ) : (
+        <div className="App" id={load ? "no-scroll" : "scroll"}>
+          <Navbar />
+          <ScrollToTop />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/project" component={Projects} />
+            <Route path="/about" component={About} />
+          </Switch>
+          <Footer />
+        </div>
+      )}
+    </Router>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>lol</div>;
 }
 
 export default App;
