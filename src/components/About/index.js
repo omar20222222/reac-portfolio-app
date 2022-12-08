ec7d1a3c185c3a27b677b6fa856c9eafa5c5078a
omar20222222 <108297683+omar20222222@users.noreply.github.com>
2022-12-08 00:22:42 +0000

    index.js added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,10 +6,17 @@ export default function About() {
     <div>
       <h1>About</h1>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat modi
-        facere suscipit aliquam possimus blanditiis, repudiandae quo expedita
-        ipsa molestiae neque aspernatur beatae, vero provident aliquid molestias
-        ab ipsam qui!
+        Hi! My name is Omar, I am 25 years old. Currently working as IT Support
+        technician Second Line for about 3 years ago. I chose to embark on this
+        Boot camp course not only for an opportunity of a career change but to
+        seek more challenges, enhance my knowledge of technology and learning in
+        general! Being in this Boot camp has taught me invaluable skills such as
+        strengthening my technical and problem-solving skills but most
+        importantly, but has given me a glimpse into the working-life of a
+        developer.
+        <p></p>I recently earned a certificate in full stack development from
+        the University of Birmingham boot camp, with newly developed skills in
+        JavaScript, CSS, React.js, and responsive web design.
       </p>
     </div>
   );
